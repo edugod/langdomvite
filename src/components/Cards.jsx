b87983aba05dc6e9c017ecf0bf07c57dee79ad72
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2023-12-19 21:36:03 -0300

    hehe

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -13,6 +13,24 @@ const Cards = () => {
                     text="Explore the hidden waterfall deep inside the amazon jungle"
                     label='adventure'
                     path='/services'/>
+                    <CardItem src='images/img-2.jpg'
+                    text="Travel through the island of bali"
+                    label='Luxury'
+                    path='/services'/>
+                </ul>
+                <ul className="cards__items">
+                    <CardItem src='images/img-9.jpg'
+                    text="Explore the hidden waterfall deep inside the amazon jungle"
+                    label='adventure'
+                    path='/services'/>
+                    <CardItem src='images/img-2.jpg'
+                    text="Travel through the island of bali"
+                    label='Luxury'
+                    path='/services'/>
+                    <CardItem src='images/img-2.jpg'
+                    text="Travel through the island of bali"
+                    label='Luxury'
+                    path='/services'/>
                 </ul>
             </div>
         </div>
